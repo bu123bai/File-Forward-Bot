f4b4b5e7b523d8a2e6e00a9cd2ee9233a06b2b02
bu123bai <120026811+bu123bai@users.noreply.github.com>
2023-05-10 22:54:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/File-Forward-Bot.git /Forward
+  git clone https://github.com/bu123bai/File-Forward-Bot.git /Forward
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Forward
 fi
 cd /Forward
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting File-Forward Bot...."
 python3 bot.py
